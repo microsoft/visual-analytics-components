98bf4e598ff565acf3ca1f6bb6693fd845ac62ff
Chris Trevino <darthtrevino@users.noreply.github.com>
2020-08-20 23:17:39 -0700

    fix: import regenerator-runtime in workers (#9)

--- packages/apps/embeddings-explorer/src/dimensionalityReduction/tsne/tsne-worker.ts
+++ packages/apps/embeddings-explorer/src/dimensionalityReduction/tsne/tsne-worker.ts
@@ -3,4 +3,5 @@
  * Licensed under the MIT license. See LICENSE file in the project.
  */
 /* eslint-disable-next-line import/no-unresolved */
+import 'regenerator-runtime'
 import './tsne-dispatcher'
