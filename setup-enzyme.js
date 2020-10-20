e530b7fa78d17e307d08072f00b47225cfead516
Chris Trevino <darthtrevino@gmail.com>
2020-10-20 16:38:11 -0700

    add header

--- setup-enzyme.js
+++ setup-enzyme.js
@@ -1,4 +1,7 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
+/*!
+ * Copyright (c) Microsoft. All rights reserved.
+ * Licensed under the MIT license. See LICENSE file in the project.
+ */
 const Enzyme = require('enzyme')
 const Adapter = require('enzyme-adapter-react-16')
 
